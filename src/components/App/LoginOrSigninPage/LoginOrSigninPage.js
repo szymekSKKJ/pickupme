5bd9263f463f740f074816662c6203332525c695
BuildTools <unconfigured@null.spigotmc.org>
2022-08-11 17:29:25 +0200

    Created redirect to login when user try to pick another user without beeing login

--- src/components/App/LoginOrSigninPage/LoginOrSigninPage.js
+++ src/components/App/LoginOrSigninPage/LoginOrSigninPage.js
@@ -1,7 +1,15 @@
 import "./LoginOrSigninPage.css";
+import Main from "../Main/Main";
+import { useEffect } from "react";
+import createNotification from "../../../customs/createNotification";
 
 const LoginOrSigninPage = () => {
-  return <div className="login-or-signin-page">???</div>;
+  useEffect(() => {
+    const loginButton = document.querySelectorAll(".main .buttons-wrapper button")[0];
+    loginButton.click();
+    createNotification("Musisz się zalogować aby móc zaczepiać innych użytkowników", "error");
+  }, []);
+  return <Main></Main>;
 };
 
 export default LoginOrSigninPage;
